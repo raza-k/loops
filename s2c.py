c4b28ef6b7ca52d54124e514510e5523d2831700
Raza Khan <raza125@outlook.com>
2021-01-30 20:17:32 -0800

    initial commit

---
+++ s2c.py
@@ -0,0 +1,5 @@
+sentence = "hello my name is raza"
+v = list(sentence)
+
+for i in v:
+    print(i)
---
+++ split.py
@@ -0,0 +1,6 @@
+sentence = "this is long an meaningless sentence"
+arr = sentence.split();
+for i in range(len(arr)):
+    v = arr[i]
+    print(i, v)
+
